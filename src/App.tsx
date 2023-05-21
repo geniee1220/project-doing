fc04fab1b206a26b21be9f6d7b4e55a23820e601
geniee1220 <geniee1220@gmail.com>
2023-05-21 18:55:36 +0900

    Update : Bundle JavaScript using dynamic import

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React, { use, useEffect } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { useGroups } from "./apis/groups";
 
-// const Home = React.lazy(() => import("./pages/Home"));
-// const StudyGroup = React.lazy(() => import("./pages/StudyGroup"));
-// const MyLounge = React.lazy(() => import("./pages/MyLounge"));
-// const Login = React.lazy(() => import("./pages/Login"));
-// const Register = React.lazy(() => import("./pages/Register"));
+const Home = React.lazy(() => import("./pages/Home"));
+const StudyGroup = React.lazy(() => import("./pages/StudyGroup"));
+const MyLounge = React.lazy(() => import("./pages/MyLounge"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Register = React.lazy(() => import("./pages/Register"));
+const GroupRecruit = React.lazy(() => import("./pages/group/GroupRecruit"));
+const GroupDetail = React.lazy(() => import("./pages/group/GroupDetail"));
 
-import Home from "./pages/Home";
-import StudyGroup from "./pages/StudyGroup";
-import MyLounge from "./pages/MyLounge";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import GroupRecruit from "./pages/group/GroupRecruit";
-import GroupDetail from "./pages/group/GroupDetail";
 import { useComments } from "./apis/comments";
 import { useRecoilState } from "recoil";
 import { userState } from "./atoms/userState";
 import GroupEdit from "./pages/group/GroupEdit";
+import Loader from "./components/atoms/Loader";
 
 function App() {
   // 유저 정보 불러오기
@@ -38,20 +34,22 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        {/* 스터디 그룹  */}
-        <Route path="/studygroup" element={<StudyGroup />} />
-        <Route path="/studygroup/recruit" element={<GroupRecruit />} />
-        <Route path="/studygroup/edit/:id" element={<GroupEdit />} />
-        <Route path="/studygroup/:id" element={<GroupDetail />} />
+          {/* 스터디 그룹  */}
+          <Route path="/studygroup" element={<StudyGroup />} />
+          <Route path="/studygroup/recruit" element={<GroupRecruit />} />
+          <Route path="/studygroup/edit/:id" element={<GroupEdit />} />
+          <Route path="/studygroup/:id" element={<GroupDetail />} />
 
-        <Route path="/lounge/my" element={<MyLounge />} />
+          <Route path="/lounge/my" element={<MyLounge />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
