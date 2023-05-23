fb4d6abc712f213d1b2f1270fe596ec6acfe1e44
geniee1220 <geniee1220@gmail.com>
2023-05-24 07:46:28 +0900

    Update : Web font

--- src/assets/styles/GlobalStyles.tsx
+++ src/assets/styles/GlobalStyles.tsx
@@ -17,6 +17,13 @@ const GlobalStyles = createGlobalStyle`
 
   html,
   body {
+    font-family: 'AppleSDGothicNeo', "Apple SD Gothic Neo",'Noto Sans KR', 
+    Arial,  "Malgun Gothic", "맑은 고딕", "Nanum Gothic", Dotum, '돋움', Helvetica, sans-serif;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
     @supports (-webkit-touch-callout: none) {
       height: -webkit-fill-available;
     }
